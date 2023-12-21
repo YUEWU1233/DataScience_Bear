4a11f554da041a5f030ae9d7bb54ab5e6d8beced
pinoulex <145569112+pinoulex@users.noreply.github.com>
2023-12-21 20:57:43 +0100

    Update PM2.5.py

--- DataCleaning/PM2.5.py
+++ DataCleaning/PM2.5.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-# Définissez le chemin d'accès au fichier CSV
-csv_file_path = "/Users/alex/Desktop/Pollution data/particulate_matter_pm2_5.csv"
+# Set path to CSV file
+csv_file_path = "Path"
 
 PM2_5_df = pd.read_csv(csv_file_path)
+
 
 print(PM2_5_df.columns)
 print(PM2_5_df["UNITS"])
@@ -11,10 +12,11 @@ PM2_5_df = PM2_5_df.loc[:, ['STATE', 'Date','Daily_Mean_PM2_5_Concentration']]
 
 PM2_5_df['Date'] = pd.to_datetime(PM2_5_df['Date'])
 PM2_5_df['Year'] = PM2_5_df['Date'].dt.year
-lead_df = PM2_5_df.drop('Date', axis=1)
-print(lead_df)
-result = lead_df.groupby(['STATE', 'Year']).mean().reset_index()
+PM25 = PM2_5_df.drop('Date', axis=1)
+print(PM25)
+
+result = PM25.groupby(['STATE', 'Year']).mean().reset_index()
 result = result.rename(columns={'Daily_Mean_PM2_5_Concentration': 'PM2.5 (in ug/m3 LC)'})
 print(result)
 
-result.to_csv('/Users/alex/Desktop/Pollution data/PM2.5.csv', index=True)
+result.to_csv('Path', index=True)
